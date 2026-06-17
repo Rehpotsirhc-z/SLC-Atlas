095df45516f0669ada6e8726576c416b21bf62f7
Rehpotsirhc <86068565+Rehpotsirhc-z@users.noreply.github.com>
2026-06-17 13:41:59 -0400

    Move TanStack Query dev button

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -24,7 +24,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <App />
         </BrowserRouter>
       </AppThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
     </QueryClientProvider>
   </React.StrictMode>,
 )
