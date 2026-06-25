4bd06413f7328ac683a031308db2a88a9a065a73
Rehpotsirhc <86068565+Rehpotsirhc-z@users.noreply.github.com>
2026-06-24 20:23:01 -0400

    Increase chunking limit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
   server: {
     host: "0.0.0.0",
     port: 3000,
